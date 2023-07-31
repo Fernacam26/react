90b7edd0d46f65ca6cd0ada237b56ce94f2e7ae9
fernacam26 <fernanc82@gmail.com>
2023-07-31 20:38:24 -0300

    form

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDragon, faCrow, /* faTwitter */} from '@fortawesome/free-solid-svg-icons'
+import { faDragon, faCrow, /* faTwitter */ } from '@fortawesome/free-solid-svg-icons'
 export default function Footer() {
   return (
     <>
@@ -12,14 +12,47 @@ export default function Footer() {
         </div>
         <div>
           <div className="grid grid-flow-col gap-4">
-           {/*   <FontAwesomeIcon icon={faTwitter} /> */}
+            {/*   <FontAwesomeIcon icon={faTwitter} /> */}
 
           </div>
         </div>
-        <div className="relative">
+        <div>
+          <form action="">
+            <div>
+              <h2 className='font-serif text-3xl'>Contact Form</h2>
+            </div>
+            <div className='text-lg'>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+              />
+            </div>
+            <div className='text-lg'>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className='text-lg'>
+              <input
+                type="number"
+                name="phonenumber"
+                placeholder="Phone Number"
+              />
+            </div>
+
+            <div>
+              <button className='btn btn-outline btn-info pt-px'>Submit Contact</button>
+            </div>
+
+          </form>
+        </div>
+        {/*         <div className="relative">
           <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
           <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Submit</button>
-        </div>
+        </div> */}
         <div>
           <p><FontAwesomeIcon icon={faCrow} />Copyright © 2023 - All right reserved by Code-Killer  <FontAwesomeIcon icon={faDragon} /></p>
         </div>
